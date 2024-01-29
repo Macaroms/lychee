ca9ce21a7be9b4a21f09f1a1e946882e9b0b1513
jiangwei <jiangwei97@aliuyun.com>
2024-01-29 16:26:18 +0800

    router index

--- lychee-web/src/router/index.js
+++ lychee-web/src/router/index.js
@@ -4,14 +4,14 @@ import { constantRouterMap, asyncRouterMap } from '@/config/router.config'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRouterMap.concat(asyncRouterMap)
 })
 
-Router.beforeEach((to, from, next) => {
+router.beforeEach((to, from, next) => {
   if(to.path.indexOf('WW_verify_S0tZA59MEWgInl15')>-1){
     let url= loadFile('/WW_verify_S0tZA59MEWgInl15.txt')
   }
@@ -19,3 +19,5 @@ Router.beforeEach((to, from, next) => {
     next()
   }
 })
+
+export default router
