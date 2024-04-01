56c9a0458e68f8d5140ad32f35a628025227c2aa
jiangwei <jiangwei97@aliuyun.com>
2024-04-01 17:33:14 +0800

    JS编辑器

--- lychee-web/src/views/text/commonEditor/jsEditor/index.vue
+++ lychee-web/src/views/text/commonEditor/jsEditor/index.vue
@@ -2,61 +2,93 @@
   <page-header-wrapper :title='false'>
     <a-card :body-style="{padding: '24px 32px'}" :bordered='false'>
       <a-spin :spinning='confirmLoading'>
-          <a-row class='form-row' :gutter='24'>
-            <a-col :lg='11' :md='11' :sm='11'>
-              <a-divider orientation='left'>JavaScript</a-divider>
-              <common-editor
-                :value="jsContent"
-                language="javascript"
-                @input="changeTextarea"
-                style="height: 80vh"
-                ref='common_editor'
-              ></common-editor>
-            </a-col>
-            <a-col :lg='2' :md='2' :sm='2' style='text-align: center;margin-top: 200px'>
-              <a-form-item :wrapper-col='{ span: 24, offset: 0 }'>
-                <a-button @click='exec' type='primary'>
+
+        <a-row class='form-row' :gutter='24'>
+          <a-col :lg='12' :md='12' :sm='12'>
+            <a-divider orientation='left'>JavaScript</a-divider>
+            <common-editor
+              :value="jsContent"
+              language="javascript"
+              @input="changeTextarea"
+              style="height: 60vh"
+              ref='common_editor'
+            ></common-editor>
+          </a-col>
+          <a-col :lg='12' :md='12' :sm='12'>
+            <a-divider orientation='left'>Params</a-divider>
+            <a-form-model
+              ref="dynamicValidateForm"
+              :model="dynamicValidateForm"
+              v-bind="formItemLayoutWithOutLabel"
+            >
+              <a-form-model-item
+                :label="$t('editor.method-name')"
+                v-bind="formItemLayout"
+                prop="methodName"
+                :rules="{required: true, message: 'methodName can not be null', trigger: 'blur'}"
+              >
+                <a-input style="width: 70%" v-model="dynamicValidateForm.methodName" :placeholder="$t('editor.method-name')" />
+              </a-form-model-item>
+              <a-form-model-item
+                v-for="(domain, index) in dynamicValidateForm.domains"
+                :key="domain.key"
+                v-bind="formItemLayout"
+                :label="'参数' + (index + 1)"
+                :prop="'domains.' + index + '.value'"
+                :rules="{required: true, message: 'domain can not be null', trigger: 'blur'}"
+              >
+                <a-input
+                  v-model="domain.value"
+                  placeholder="please input domain"
+                  style="width: 45%; margin-right: 8px"
+                />
+                <a-select v-model="domain.type" style="width: 24%; margin-right: 8px">
+                  <a-select-option v-for="type in types" :key="type.value">
+                    {{ type.name }}
+                  </a-select-option>
+                </a-select>
+                <a-icon
+                  class="dynamic-delete-button"
+                  type="minus-circle-o"
+                  @click="removeDomain(domain)"
+                />
+              </a-form-model-item>
+              <a-form-model-item v-bind="formItemLayoutWithOutLabel">
+                <a-button type="dashed" style="width: 60%" @click="addDomain">
+                  <a-icon type="plus" />
+                  添加参数
+                </a-button>
+              </a-form-model-item>
+              <a-form-model-item v-bind="formItemLayoutWithOutLabel">
+                <a-button type="primary" html-type="submit" @click="submitForm()">
                   <a-icon type="caret-right" />
                   {{ $t('editor.execute') }}
                 </a-button>
-              </a-form-item>
-              <a-form-item :wrapper-col='{ span: 24, offset: 0 }'>
-                <a-button @click='reset' type='danger'>
+                <a-button style="margin-left: 10px" type='danger' @click="reset()">
                   <a-icon type="undo" />
                   {{ $t('editor.reset') }}
                 </a-button>
-              </a-form-item>
-            </a-col>
-            <a-col :lg='11' :md='11' :sm='11'>
-              <a-divider orientation='left'>Result</a-divider>
-                <a-row>
-                  <a-col :lg='12' :md='12' :sm='12'>
-                    <a-form-item >
-                      <a-input style="width: 95%" v-model="methodName" :placeholder="$t('editor.method-name')" />
-                    </a-form-item>
-                  </a-col>
-                  <a-col :lg='12' :md='12' :sm='12'>
-                    <a-form-item >
-                      <a-input style="width: 95%" v-model="methodArgs" :placeholder="$t('editor.args')" />
-                    </a-form-item>
-                  </a-col>
-                </a-row>
-              <a-row>
-              <a-form-item
-                :labelCol='labelCol'
-                :wrapperCol='wrapperCol'
-              >
-                <a-textarea
-                  style="height: calc(80vh - 130px);background-color: #0c1021;color: #8da6ce"
-                  read-only
-                  v-model='result'
-                />
-              </a-form-item>
-              </a-row>
-            </a-col>
-          </a-row>
+              </a-form-model-item>
+            </a-form-model>
+          </a-col>
+        </a-row>
       </a-spin>
     </a-card>
+    <a-modal v-model="visible" title="执行结果" width="800px">
+      <template slot="footer">
+        <a-button key="back" @click="handleCancel">
+          关闭
+        </a-button>
+      </template>
+                    <a-form-item
+                    >
+                      <a-textarea
+                        style="height: 400px;background-color: #0c1021;color: #8da6ce"
+                        read-only
+                        v-model='result'
+                      />
+                    </a-form-item>
+    </a-modal>
   </page-header-wrapper>
 </template>
 
@@ -96,8 +128,48 @@ export default {
         'function getNum(num){\n' +
         '  return num + 1;\n' +
         '}',
-      methodName: 'main',
-      methodArgs: 'wei, 2',
+      formItemLayout: {
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 4 }
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 20 }
+        }
+      },
+      formItemLayoutWithOutLabel: {
+        wrapperCol: {
+          xs: { span: 24, offset: 0 },
+          sm: { span: 20, offset: 4 }
+        }
+      },
+      dynamicValidateForm: {
+        methodName: 'main',
+        domains: [
+          {
+            value: 'wei',
+            type: 'text',
+            key: 1
+          },
+          {
+            value: '1',
+            type: 'number',
+            key: 2
+          }
+        ]
+      },
+      types: [
+        {
+          name: '文本',
+          value: 'text'
+        },
+        {
+          name: '数字',
+          value: 'number'
+        }
+      ],
+      visible: false,
     }
   },
   methods: {
@@ -106,35 +178,59 @@ export default {
       this.$refs.common_editor.setCodeContent('')
       this.jsContent = ''
       this.result = ''
+      this.dynamicValidateForm.methodName = ''
+      this.dynamicValidateForm.domains= []
       this.confirmLoading = false
-    },
-    exec() {
-      this.confirmLoading = true
-      let arr = this.methodArgs.split(',')
-      let args = []
-      for(let a of arr){
-        args.push(a.trim())
-      }
-      postAction(
-        this.url.execJsScript,
-        {
-          code: this.jsContent,
-          method: this.methodName,
-          args: args
-        }
-      ).then((res) => {
-        if (res.code === 200) {
-          this.result = res.data
-        } else {
-          this.$message.error(res.message)
-        }
-      }).finally(() => {
-        this.confirmLoading = false
-      })
     },
     changeTextarea(val) {
       this.jsContent = val
-    }
+    },
+    submitForm() {
+      this.$refs['dynamicValidateForm'].validate(valid => {
+        if (valid) {
+          this.confirmLoading = true
+          this.showModal()
+          postAction(
+            this.url.execJsScript,
+            {
+              code: this.jsContent,
+              method: this.dynamicValidateForm.methodName,
+              args: this.dynamicValidateForm.domains
+            }
+          ).then((res) => {
+            if (res.code === 200) {
+              this.result = res.data
+            } else {
+              this.$message.error(res.message)
+            }
+          }).finally(() => {
+            this.confirmLoading = false
+          })
+        } else {
+          return false
+        }
+      })
+    },
+    removeDomain(item) {
+      let index = this.dynamicValidateForm.domains.indexOf(item)
+      if (index !== -1) {
+        this.dynamicValidateForm.domains.splice(index, 1)
+      }
+    },
+    addDomain() {
+      this.dynamicValidateForm.domains.push({
+        value: '',
+        type: 'text',
+        key: Date.now()
+      })
+    },
+    showModal() {
+      this.visible = true;
+    },
+    handleCancel(e) {
+      this.visible = false;
+      this.result = ''
+    },
   }
 }
 </script>
